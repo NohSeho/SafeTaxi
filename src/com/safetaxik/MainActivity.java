6dcfe1bf0c25ef750193d5f8d52398412ede899f
SehoNoh <nohseho2@icloud.com>
2013-10-26 02:09:33 +0900

    Fuck

--- src/com/safetaxik/MainActivity.java
+++ src/com/safetaxik/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		startActivity(new Intent(MainActivity.this, PatternLockActivity.class));
 	}
 
 	@Override
